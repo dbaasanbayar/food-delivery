b79f9c2e7035f01ac84b51f518b33f320fb318e1
dbaasanbayar <d.baasanbayar@gmail.com>
2025-11-03 21:20:39 +0800

    generating token

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -1,10 +1,15 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { UserModel } from "../models/user.js";
 
 export const signUp = async (req, res) => {
   try {
-    const { body } = req;
-    const { email, password } = body;
+    const { email, password } = req.body;
+    const token = jwt.sign(
+      { username: req.body.email },
+      { password: req.body.password }
+    );
+    console.log(token, "token");
 
     const hashedPassword = await bcrypt.hashSync(password, 1);
     console.log("old password", password);
@@ -19,4 +24,12 @@ export const signUp = async (req, res) => {
     console.log("error", error);
     res.status(500).send({ message: "Error", error });
   }
+};
+
+export const SignIn = async (req, res) => {
+  try {
+    const result = await UserModel.find(req.params.id);
+    const token = jwt.sign(result);
+    console.log();
+  } catch (error) {}
 };
