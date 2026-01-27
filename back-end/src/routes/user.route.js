fab8921ff3312ea799a324bcb0021e3333c3de56
dbaasanbayar <d.baasanbayar@gmail.com>
2026-01-27 16:32:30 +0800

    changes

--- back-end/src/routes/user.route.js
+++ back-end/src/routes/user.route.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { signIn, signUp } from "../controllers/user.js";
-import { verifyToken } from "../middleware/auth.js";
 
 export const userRouter = Router();
 
